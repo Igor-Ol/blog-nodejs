3fd119d4e2a150e39e6e1408c839f071845a3755
IGOR OLIVEIRA PEREIRA <igor.opereira4@senacsp.edu.br>
2025-08-13 17:25:42 -0300

    Aula-02

--- app.js
+++ app.js
@@ -11,12 +11,22 @@ app.listen(3001);
 
 //acessar uma rota 
 app.get('/', (req, res) => {
-    res.render('index');
+
+    //passando parâmetros para o body
+    const blogs = [
+       {titulo: 'Charmander', conteudo: 'A ponta do seu rabo está constantemente em chamas e se for apagada pode resultar em sua morte.'},                
+       {titulo: 'Squirtle', conteudo: 'Seu casco reduz a resistência contra a água fazendo com que nade mais rápido.'},
+       {titulo: 'Bulbasaur', conteudo: 'Ao evoluir o bulbo começa a desabrochar até se tornar uma grande flor na evolução final.'},
+
+    ];
+
+
+    res.render('index', {titulo: 'Home', blogs});
 });
 
 //nova rota 
 app.get('/sobre', (req, res) => {
-    res.render('sobre');
+    res.render('sobre', {titulo: 'Sobre'});
 });
 
 //redirecionamento de página
@@ -24,7 +34,12 @@ app.get('/sobrenos', (req, res) => {
     res.redirect('/sobre')
 });
 
+//rota da criação do conteúdo
+app.get('/blog/criar', (req, res) => {
+    res.render('criar', {titulo: 'Criar novo Blog'});
+});
+
 //erro 404
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {titulo: '404'});
 })
